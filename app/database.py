949f248c6384c6890a039dfd94720c2e5962c557
Lekhana <lekhanapatel6@gmail.com>
2024-11-06 21:16:35 +0530

    database edit2

--- app/database.py
+++ app/database.py
@@ -8,8 +8,8 @@ import os
 settings=Settings()
 
 database_url = os.getenv('DATABASE_URL', f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
-if database_url.startswith("postgres://"):
-    database_url = database_url.replace("postgres://", "postgresql://", 1)
+# if database_url.startswith("postgres://"):
+#     database_url = database_url.replace("postgres://", "postgresql://", 1)
 engine=create_engine(database_url)
 
 SessionLocal=sessionmaker(autocommit=False, autoflush=False,bind=engine)
