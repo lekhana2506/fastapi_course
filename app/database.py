46ff66c2cddd7e601b4f4ae759e0a28b2112d543
Lekhana <lekhanapatel6@gmail.com>
2024-11-06 20:17:25 +0530

    changed from postgresql to postgres

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ import os
 
 settings=Settings()
 
-database_url=database_url = os.getenv('DATABASE_URL') or f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+database_url=database_url = os.getenv('DATABASE_URL') or f'postgres://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine=create_engine(database_url)
 
