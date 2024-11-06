deb011985e1d9c4653aa3886233bd6bdc746e9b7
Lekhana <lekhanapatel6@gmail.com>
2024-11-06 20:23:12 +0530

    changed database.py

--- app/database.py
+++ app/database.py
@@ -2,12 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import Settings
+import psycopg2
 import os
 
 settings=Settings()
 
-database_url=database_url = os.getenv('DATABASE_URL') or f'postgres://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
-
+database_url = os.getenv('DATABASE_URL', f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
 engine=create_engine(database_url)
 
 SessionLocal=sessionmaker(autocommit=False, autoflush=False,bind=engine)
