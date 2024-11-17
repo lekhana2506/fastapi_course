38d5cd8c58ec35a98bc58141dc1c1d89eb580cd7
Lekhana <lekhanapatel6@gmail.com>
2024-11-17 19:38:19 +0530

    removed app import

--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,4 @@
 from fastapi.testclient import TestClient
-from app.main import app
 from app.config import Settings
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
